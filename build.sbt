63dd98c4db0e9d34b2330e1ac98aeb445fda0937
trampolo <rrotta@gmail.com>
2013-10-15 12:29:33 +0200

    "p440"

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "Project Euler Solutions"
+
+version := "0.1"
+
+scalaVersion := "2.10.3"
+
+scalacOptions ++= Seq("-deprecation", "-feature")
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.2" % "test"
+
+libraryDependencies += "junit" % "junit" % "4.11" % "test"
---
+++ src/test/scala/problems/P440Suite.scala
@@ -0,0 +1,19 @@
+package problems
+
+import org.scalatest._
+import scala.math.BigInt
+
+class P440Suite extends FunSuite {
+
+
+	test("T(n) be the number of ways to tile a board of length n") {
+		assert(new Solver(1).tsolutions === 10)
+		assert(new Solver(2).tsolutions === 101)
+	}
+
+	test("S(L) be the triple sum ∑a,b,c gcd(T(ca), T(cb)) for 1 ≤ a, b, c ≤ L") {
+		assert(new Solver(2).ssolutions === 10444)
+		assert(new Solver(3).ssolutions === BigInt("1292115238446807016106539989"))
+	}
+
+}
