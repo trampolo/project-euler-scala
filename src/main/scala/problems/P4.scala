3872246ee7084786607175cee888d0e552dcdb20
trampolo <rrotta@gmail.com>
2013-10-17 14:54:19 +0200

    reformat P4

--- src/main/scala/problems/P4.scala
+++ src/main/scala/problems/P4.scala
@@ -4,11 +4,11 @@ case class SolverP4(n: Int) {
 
 	def isPalindrome(x: Int) = x.toString.reverse == x.toString
 
-lazy val solution = for {
-	a <- (n to 100 by -1)
-	b <- (n to 100 by -1)
-	if (isPalindrome(a * b)) 
-} yield (a * b, a, b)
+	lazy val solution = for {
+		a <- (n to 100 by -1)
+		b <- (n to 100 by -1)
+		if (isPalindrome(a * b)) 
+	} yield (a * b, a, b)
 }
 
 object P4 extends App {
