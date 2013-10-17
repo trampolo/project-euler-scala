2e6abc92399cc1e184d010f289a3dc7fdded211e
trampolo <rrotta@gmail.com>
2013-10-17 17:46:10 +0200

    added P6, P7, P8, P9, P10

---
+++ src/main/scala/problems/P9.scala
@@ -0,0 +1,25 @@
+/*
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2 For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find
+the product abc. 
+
+*/
+
+package problems
+
+object P9 extends App {
+
+println(
+	(for {
+		a <- 1 to 1000
+		b <- 1 to (1000 - a)
+		c <- 1 to (1000 - b - a)
+		if (a + b + c) == 1000
+		if (a*a + b*b) == c*c
+	} yield a * b * c).head
+	)
+}
